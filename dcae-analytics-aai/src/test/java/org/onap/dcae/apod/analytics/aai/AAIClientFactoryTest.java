77524bac314aec4894b86e352d4a08f1f7e9f090
rajendrajaiswal <rajendra.jaiswal@ericsson.com>
2019-06-27 10:50:55 +0100

    JJB alignment across DCAE components - DCAE-TCA
    Change-Id: Ife5462fc6b5c6b007fb892b77d65f4138f50054b
    Issue-ID: DCAEGEN2-1578
    Signed-off-by: rajendrajaiswal <rajendra.jaiswal@ericsson.com>

--- dcae-analytics-aai/src/test/java/org/onap/dcae/apod/analytics/aai/AAIClientFactoryTest.java
+++ dcae-analytics-aai/src/test/java/org/onap/dcae/apod/analytics/aai/AAIClientFactoryTest.java
@@ -1,19 +1,12 @@
 package org.onap.dcae.apod.analytics.aai;
-
-import static org.junit.Assert.assertNotNull;
-
 import org.junit.Test;
-
+import static org.junit.Assert.assertNotNull;
 
 public class AAIClientFactoryTest {
 
-	
-
-	@Test
-	public void testCreate_shouldReturn_NotNull() {
-		AAIClientFactory result = AAIClientFactory.create();
-		assertNotNull(result);
-
-	}
-
+@Test
+public void testCreate_shouldReturn_NotNull() {
+    AAIClientFactory result = AAIClientFactory.create();
+    assertNotNull(result);
+    }
 }
